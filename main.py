ea527b1fc47e841eb4d3be574bfb0f149b931b1f
Benciorno <107666737+Benciorno@users.noreply.github.com>
2022-06-21 11:19:49 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# 1
+# a = dict()
+# n = int(input("До какого числа возводить в квадрат: "))
+# for i in range(1, n):
+#     a[i] = i * i
+# print(a)
+
+# 2
+# k = input("Ключи: ").split()
+# v = list(map(int,input("Значения: ").split()))
+# a = dict(zip(k, v))
+# print(a)
+# print(f"Сумма значений: {sum(a.values())}")
+# print(f"Среднее арифметическое: lambda {sum(a.values()) / len(v)}")
+
+# 3
+# k = input("Ключи: ").split()
+# v = input("Значения: ").split()
+# a = dict(zip(k, v))
+# print(a)
+
+# 5
+# goods = {
+#     'Лампа': '12345',
+#     'Стол': '23456',
+#     'Диван': '34567',
+#     'Стул': '45678',
+# }
+# store = {
+#     '12345': [
+#         {'quantity': 27, 'price': 42},
+#     ],
+#     '23456': [
+#         {'quantity': 22, 'price': 510},
+#         {'quantity': 32, 'price': 520},
+#     ],
+#     '34567': [
+#         {'quantity': 2, 'price': 1200},
+#         {'quantity': 1, 'price': 1150},
+#     ],
+#     '45678': [
+#         {'quantity': 50, 'price': 100},
+#         {'quantity': 12, 'price': 95},
+#         {'quantity': 43, 'price': 97},
+#     ],
+# }
